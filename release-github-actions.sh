64044b0685b8b18d7def51030b8ab26f80984b93
Jason Froehlich <38667521+jayfray12@users.noreply.github.com>
2021-05-03 10:11:07 -0400

    Update release-github-actions.sh

--- release-github-actions.sh
+++ release-github-actions.sh
@@ -3,4 +3,10 @@
 export CI_COMMIT_SHA="${GITHUB_SHA}"
 export CI_COMMIT_REF_NAME="${GITHUB_REF}"
 
+gpg --list-secret-keys --keyid-format LONG
+
+cat /github/workspace/.github/workflows/settings.xml
+
+echo "GPG KEY ${GPG_KEY_ID}"
+
 release.sh
